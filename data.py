9641688ba086436612513c477531388c7dc6d116
ben-vow-repo <benjaminvowles2008@gmail.com>
2026-08-18 19:38:06 +0000

    buy and sell mechanics created

--- data.py
+++ data.py
@@ -10,9 +10,12 @@ class Stocks:
         self.max_price = 40
         self.volatility = 0.2
 
+    def increase_price(self):
+        
+
 
 class Trader:
-    def __init__(self, id ,minimum_buy_price, maximum_buy_price, minimum_sell_price, maximum_sell_price, risk_tolerance, cash_spend_percentage, total_sell_percentage):
+    def __init__(self, id ,minimum_buy_price, maximum_buy_price, minimum_sell_price, maximum_sell_price, risk_tolerance, cash_spend_percentage, total_sell_percentage, greediness):
         self.id = id
         self.cash_balance = 1000
         self.starting_balance = 1000
@@ -26,21 +29,74 @@ class Trader:
         self.total_sell_percentage = total_sell_percentage
         self.profit_loss = 0
         self.transaction_history = []
+        self.portfolio = []
+        self.greediness = greediness
+
+
+    def sell_stock(self, stock, holding, market):
+        """ defines whether or not a stock should be sold using the trader's min/max selling prices and the profit to be gained from the trade"""
+        if stock.current_price < self.minimum_sell_price or stock.current_price > self.maximum_sell_price:
+            return False
+        current_value = stock.current_price*holding.quantity
+        original_cost = holding.price_bought_at*holding.quantity
+        profit = original_cost - current_value
+
+        if stock.current_price >= stock.max_price*self.greediness and profit>0:
+            return True
+
+        return False
+
+    def buy_stock(self, stock, market):
+        """defines whether or not a stock should be bought using the trader's min/max selling price and the risk of purchasing the stock"""
+        if stock.current_price < self.minimum_buy_price or stock.current_price > self.maximum_buy_price:
+            return False
+        if stock.volatility > self.risk_tolerance:
+            return False
+
+        amount_spendable = self.cash_balance*self.cash_spend_percentage
+
+        if amount_spendable < stock.current_price:
+            return False:
+
+        quantity = int(amount_spendable / stock.current_price)
+        self.cash_balance -= quantity*stock.current_price
+        purchase = StocksBought(
+            stock_id=stock.id,
+            price_bought_at=stock.current_price,
+            quantity=quantity,
+            trader_id=self.id,
+            day_bought=market.current_day
+        )
+        self.portfolio.append(purchase)
+        if market.current_day not in market.transaction_log:
+            market.transaction_log[market.curent_day] = {}
+        if self.id not in market.transaction_log[market.current_day]:
+            market.transaction_log[market.current_day][self.id] = []
+        market.transaction_log[market.current_day][self.id].append({
+            'trader_id': self.id,
+            'stock_id': stock.id,
+            'action': ' buy',
+            'price' : stock.current_price,
+            'total_cost': quantity*stock.current_price
+        })
+        return True
 
 
 
+class StocksBought:
 
-class Stocks_bought:
-    def __init__(self, id, price_bought_at, quantity, trader_id):
-        self.id = id
+    def __init__(self, stock_id, price_bought_at, quantity, trader_id, day_bought):
+        self.stock_id = stock_id
         self.price_bought_at = price_bought_at
         self.quantity = quantity
         self.trader_id = trader_id
+        self.day_bought = day_bought
+
 
 class Market:
     def __init__(self):
         self.current_day = 0
         self.all_stocks = []
         self.all_traders = []
-        self.transaction_log = []
+        self.transaction_log = {}
         self.stock_prices_over_time = {}
